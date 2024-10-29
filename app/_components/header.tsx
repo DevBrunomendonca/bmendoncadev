3e4eb047a19b3c6a3ab7526a76ee5482f9dfbd27
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-29 18:05:25 -0300

    style: update style text color

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,20 +1,16 @@
 import Logo from "./logo"
-import {
-  IconSocialMedia,
-  ListIconSocialMedias,
-} from "./list-icon-social-medias"
+import { IconSocialMedia, ListIcons } from "./list-icons"
 import SheetButton from "./sheet-button"
 import ListNavHeader from "./list-nav-header"
 
 const Header = () => {
   return (
     <header
-      className="fixed left-0 right-0 top-0"
+      className="fixed left-0 right-0 top-0 z-50 border-third-blue"
       style={{
-        backdropFilter: "blur(7px)",
-        WebkitBackdropFilter: "blur(7px)",
-        background: "rgba(8, 15, 32, 0.01)",
-        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.17)",
+        backdropFilter: "blur(8px)",
+        WebkitBackdropFilter: "blur(8px)",
+        background: "rgba(8, 15, 32, 0.55)",
       }}
     >
       <div className="mx-auto flex w-full max-w-6xl justify-between p-4">
@@ -22,33 +18,33 @@ const Header = () => {
         <div className="hidden items-center gap-4 md:flex">
           <ListNavHeader />
           <div className="h-full w-[1px] bg-gray-50 opacity-15"></div>
-          <ListIconSocialMedias className="flex h-4 items-center gap-3">
+          <ListIcons className="flex h-4 items-center gap-3">
             <IconSocialMedia
               href="https://www.instagram.com/brunmendonca_/"
-              srcIcon="icone-insta.svg"
+              srcIcon="/icons/icone-insta.svg"
               altIcon="Icone Instagra"
             />
             <IconSocialMedia
               href="https://www.linkedin.com/in/devbrunomendonca/"
-              srcIcon="icone-linkdin.svg"
+              srcIcon="/icons/icone-linkdin.svg"
               altIcon="Icone LinkdIn"
             />
             <IconSocialMedia
               href="https://github.com/DevBrunomendonca"
-              srcIcon="icone-github.svg"
+              srcIcon="/icons/icone-github.svg"
               altIcon="Icone Github"
             />
             <IconSocialMedia
               href="https://wa.link/n2kytx"
-              srcIcon="icone-wtpp.svg"
+              srcIcon="/icons/icone-wtpp.svg"
               altIcon="Icone WhatsApp"
             />
             <IconSocialMedia
               href="mailto:devbrunomendonca@gmail.com"
-              srcIcon="icone-email.svg"
+              srcIcon="/icons/icone-email.svg"
               altIcon="Icone Email"
             />
-          </ListIconSocialMedias>
+          </ListIcons>
           <div className="flex h-4 items-center gap-3"></div>
         </div>
         <SheetButton />
--- app/_components/list-nav-header.tsx
+++ app/_components/list-nav-header.tsx
@@ -9,25 +9,25 @@ const ListNavHeader = () => {
     <nav className="flex items-center gap-4">
       <ul className="contents">
         <Link
-          className={`${pathName === "/" ? "hover:none font-medium text-secondary-blue" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
+          className={`${pathName === "/" ? "hover:none font-medium text-primary-white" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
           href="/"
         >
           <li>Home</li>
         </Link>
         <Link
-          className={`${pathName === "/projetos" ? "hover:none font-medium text-secondary-blue" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
+          className={`${pathName === "/projetos" ? "hover:none font-medium text-primary-white" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
           href="/projetos"
         >
           <li>Projetos</li>
         </Link>
         <Link
-          className={`${pathName === "/contato" ? "hover:none font-medium text-secondary-blue" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
+          className={`${pathName === "/contato" ? "hover:none font-medium text-primary-white" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
           href="/contato"
         >
           <li>Contato</li>
         </Link>
         <Link
-          className={`${pathName === "/blog" ? "hover:none font-medium text-secondary-blue" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
+          className={`${pathName === "/blog" ? "hover:none font-medium text-primary-white" : "font-normal text-gray-400 hover:text-gray-200"} w-full cursor-pointer text-sm transition-all`}
           href="/blog"
         >
           <li>Blog</li>
