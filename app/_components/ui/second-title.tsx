be74a25953b60335cdc2fffa8f9c0356998c77dd
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2024-10-28 22:45:51 -0300

    feat: add component SecondTitle

---
+++ app/_components/ui/second-title.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/app/_lib/utils"
+
+interface SecondoTitleProps {
+  content: string
+  className?: string
+}
+
+const SecondTitle = ({ content, className }: SecondoTitleProps) => {
+  return (
+    <h2 className={cn("text-xl font-medium text-primary-white", className)}>
+      {content}
+    </h2>
+  )
+}
+
+export default SecondTitle
